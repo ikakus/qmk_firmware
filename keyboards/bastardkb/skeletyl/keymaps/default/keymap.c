6230529d53bc735a8b48c52c4182a4b09b1195cb
ikakus <idadiani88@gmail.com>
2021-08-30 22:48:39 +0300

    skeletyl changes

--- keyboards/bastardkb/skeletyl/keymaps/default/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/default/keymap.c
@@ -27,20 +27,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, 
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                                MO(2), KC_SPC, KC_LCTRL,        KC_LALT, KC_ENT, MO(1)
+                                                MO(2), KC_SPC, KC_LCTRL,        KC_RSHIFT, KC_ENT, MO(1)
                                           //`--------------------------'  `--------------------------'
 
       ),
 
   [1] = LAYOUT_split_3x5_3(
       //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-            XXXXXXX, XXXXXXX,    KC_1,    KC_2,    KC_3,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+           KC_GESC,    KC_1,    KC_2,    KC_3,  XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            XXXXXXX, XXXXXXX, KC_4,     KC_5,       KC_6,                                KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSPACE,
+           XXXXXXX, KC_4,     KC_5,       KC_6, XXXXXXX,                                 KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSPACE,
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-            XXXXXXX, KC_0,    KC_7,     KC_8,       KC_9,                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+          KC_0,    KC_7,     KC_8,       KC_9,  XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                             MO(2), KC_LSHIFT, KC_LCTRL,        KC_LALT, KC_ENT, _______
+                                             MO(3), KC_LSHIFT, KC_LCTRL,       KC_RSHIFT, KC_ENT, _______
                                         //`--------------------------'  `--------------------------'
   ),
 
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                              KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+      KC_LCTRL, KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX,                         KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        _______, KC_SPC, KC_LCTRL,        KC_LALT, KC_ENT, MO(1)
+                                        _______, KC_SPC, KC_LCTRL,        KC_LALT, KC_ENT, MO(3)
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -60,11 +60,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_MS_BTN2, KC_MS_BTN1, XXXXXXX,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                                _______, KC_SPC, KC_LCTRL,        KC_LALT, KC_ENT, _______
+                                        _______, KC_SPC, KC_LCTRL,        KC_LALT, KC_ENT, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
