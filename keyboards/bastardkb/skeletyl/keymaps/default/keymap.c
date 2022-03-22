ff4790c8a1467f66e3f9abc8c61eced750b95496
irakli dadiani <irakli.dadiani@changeinvest.com>
2022-03-22 12:10:21 +0200

    update keymap

--- keyboards/bastardkb/skeletyl/keymaps/default/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       CTL_T(KC_A), KC_S,    KC_D,    KC_F,    KC_G,                                     KC_H,    KC_J,    KC_K,    KC_L, CTL_T(KC_SCLN),
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      SFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M, KC_COMM,  KC_DOT, SFT_T(KC_SLSH), 
+      SFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,                                  KC_N,    KC_M, KC_COMM,  KC_DOT, SFT_T(KC_SLSH),
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_LCTRL, LT(2, KC_SPC),KC_LALT ,         KC_LGUI, MO(1), KC_ENT
                                           //`--------------------------'  `--------------------------'
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
            KC_GESC, KC_4, KC_5, KC_6, KC_0,                                         KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSPACE,
       //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-           KC_LSHIFT, KC_7, KC_8, KC_9, XXXXXXX,                                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSHIFT,
+           KC_LSHIFT, KC_7, KC_8, KC_9, XXXXXXX,                                     CG_TOGG , XXXXXXX, XXXXXXX, XXXXXXX, KC_RSHIFT,
       //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_LCTRL, MO(4),KC_LALT ,         KC_LGUI, KC_NO, KC_ENT
                                        //`--------------------------'  `--------------------------'
@@ -66,16 +66,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         KC_LCTRL, KC_NO ,KC_LALT ,         KC_LGUI, KC_NO, KC_ENT
                                       //`--------------------------'  `--------------------------'
   ),
-  
+
   [4] = LAYOUT_split_3x5_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F10,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_F4,KC_F5 , KC_F6, KC_F11,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LCTRL, KC_F4, KC_F5 , KC_F6, KC_F11,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_F7,KC_F8 ,KC_F9 , KC_F12,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_LSHIFT, KC_F7,KC_F8 ,KC_F9 , KC_F12,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_LCTRL, KC_NO ,KC_LALT ,         KC_LGUI, KC_NO, KC_ENT
                                       //`--------------------------'  `--------------------------'
-  ) 
+  )
 };
--- keyboards/lily58/keymaps/ikakus/keymap.c
+++ keyboards/lily58/keymaps/ikakus/keymap.c
@@ -18,7 +18,7 @@ char term_symbol_visible [ ] = ">_ ";
 char term_symbol_invisible [ ] = ">  ";
 char term_symbol [] = "   ";
 bool underscore_visible = true;
-bool keylogger_enabled = false; 
+bool keylogger_enabled = false;
 
 #define _QWERTY 0
 #define _LOWER 1
@@ -35,7 +35,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_LGUI, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LCTL, KC_LALT, MO(2), KC_SPC, KC_ENT, MO(1), KC_RALT, KC_RCTL),
-	
+
 [_LOWER] = LAYOUT(KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_F13, KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_NO, KC_NO, KC_MRWD, KC_MFFD, KC_MPLY, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSPC, KC_DEL, KC_TRNS, KC_NO, KC_NO, KC_VOLD, KC_VOLU, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_PGUP, KC_PGDOWN, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS),
 
 
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void matrix_init_user(void) {
     //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
     #ifdef SSD1306OLED
-        iota_gfx_init(!has_usb());   // turns on the display
+     //iota_gfx_init(!has_usb());   // turns on the display
     #endif
-    
+
     strcpy(term_symbol, term_symbol_visible);
     timer = timer_read32();
 }
@@ -72,22 +72,22 @@ void matrix_render_user(struct CharacterMatrix *matrix) {
   if (is_master) {
     // If you want to change the display of OLED, you need to change here
 
-    matrix_write_ln(matrix, read_layer_state());
+    //matrix_write_ln(matrix, read_layer_state());
     //matrix_write_ln(matrix, "");
     matrix_write(matrix, term_symbol);
-    
+
     if(keylogger_enabled == true){
-    	matrix_write_ln(matrix, read_keylogs());
+    //	matrix_write_ln(matrix, read_keylogs());
     }else{
-    	matrix_write_ln(matrix, read_keylog());
+    //	matrix_write_ln(matrix, read_keylog());
     }
-    
+
   } else {
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, "");
+    //matrix_write_ln(matrix, read_layer_state());
+    //matrix_write_ln(matrix, "");
     matrix_write(matrix, term_symbol);
-    matrix_write_ln(matrix, read_keylog());
-    
+    //matrix_write_ln(matrix, read_keylog());
+
   }
 }
 
@@ -103,8 +103,8 @@ void iota_gfx_task_user(void) {
   matrix_clear(&matrix);
   matrix_render_user(&matrix);
   matrix_update(&display, &matrix);
-  
-  
+
+
   if (timer_elapsed32(timer) > 500) {
   	    timer = timer_read32();
   		if(underscore_visible == true){
@@ -164,7 +164,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-   
+
   }
   return true;
 }
